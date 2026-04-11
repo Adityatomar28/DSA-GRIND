007ba786f598b61f6605b768fae711fcd90d5d21
Aditya Singh Tomar <151534385+Adityatomar28@users.noreply.github.com>
2026-04-11 06:12:48 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& cost,vector<int>&dp) {
+
+        if(n == 0) return cost[0];
+        if(n == 1) return cost[1];
+
+        if(dp[n] != -1) return dp[n];
+
+        // Recursive relation
+        int ans1 = solve(n-1, cost,dp);
+        int ans2 = solve(n-2, cost,dp);
+
+        return dp[n] = min(ans1, ans2) + cost[n];
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n,-1);
+        return min(
+            solve(n-1, cost,dp),
+            solve(n-2, cost,dp)
+        );
+    }
+};
