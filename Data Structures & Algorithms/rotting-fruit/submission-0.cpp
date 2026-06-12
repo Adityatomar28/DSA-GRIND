78026545c76ac787ddb7226ea0afbfb86698baca
Aditya Singh Tomar <151534385+Adityatomar28@users.noreply.github.com>
2026-06-13 01:31:23 +0530

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    void bfs(queue<pair<int,int>>& q,vector<vector<int>>& grid,
+        int& fresh,int& time) {
+    int n = grid.size();
+    int m = grid[0].size();
+
+    int dr[] = {-1,0,1,0}; 
+    int dc[] = {0,1,0,-1};
+
+    while(!q.empty()){
+    int size = q.size();
+    bool rotten = false;
+
+    for(int i=0;i<size;i++){
+        auto[r,c] = q.front();
+        q.pop();
+
+        for(int k=0;k<4;k++){
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+
+            if (nr >= 0 && nr < n &&
+                        nc >= 0 && nc < m &&
+                        grid[nr][nc] == 1) {
+            grid[nr][nc] = 2;
+            fresh--;
+
+            q.push({nr, nc});
+            rotten = true;
+
+
+            
+            }
+        }
+    }
+    if (rotten)
+    time++;
+    }
+
+
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+
+    queue<pair<int,int>>q;
+    int fresh = 0;
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+        if(grid[i][j] == 2)
+          q.push({i,j});
+        else if (grid[i][j] == 1)
+          fresh++;
+        }
+    }
+    if(fresh == 0)
+    return 0;
+
+    int time = 0;
+    bfs(q,grid,fresh,time);
+
+    return (fresh == 0) ? time:-1;
+
+    }
+};
